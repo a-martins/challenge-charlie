944e519733377e126127c05cd910a81cd99e48d4
Alexandre Martins <alexandremartins.ti@gmail.com>
2024-07-17 21:25:20 -0300

    chore: removing react-query devtool

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 import { WeatherProvider } from "./contexts/WeatherProvider";
 import queryClient from "./queries/QueryClient";
 import Router from "./routes/Router";
@@ -12,7 +11,6 @@ export default class App extends React.Component {
         <WeatherProvider>
           <QueryClientProvider client={queryClient}>
             <Router />
-            <ReactQueryDevtools initialIsOpen={true} />
           </QueryClientProvider>
         </WeatherProvider>
       </React.StrictMode>

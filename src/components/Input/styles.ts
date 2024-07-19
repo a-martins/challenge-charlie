87da89e6aeb840c9539cd2eea607a400fb146eb7
Alexandre Martins <alexandremartins.ti@gmail.com>
2024-07-19 15:46:11 -0300

    feat: adding underline on input text field

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -9,9 +9,11 @@ const InputContainer = styled.div`
 
 const InputField = styled.input`
   width: 100%;
+  background: linear-gradient(#808080, #808080) center bottom 1px /
+    calc(100% - 10px) 2px no-repeat;
   background-color: rgb(255, 255, 255, 0);
-  margin-left: 8px;
   border: none;
+  margin-left: 8px;
   font-size: 24px;
   font-weight: 700;
   height: 32px;
